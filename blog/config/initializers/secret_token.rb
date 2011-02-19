624c37bd72fbfddd24d8a1e1f23aee1c5f013547
niczka <dominikarogozinska@gmail.com>
2011-02-19 13:34:06 +0100

    db scheme

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'de36a86cc7767efe7aeb80fed4c42eab01445af3e811261840504e172a5bd7aff74ec353d2495f306af276b0f7a204548ab6811dcd42c787cd31de9948c4ca32'
